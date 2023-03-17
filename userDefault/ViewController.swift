56c5c074806821cfda8845e7a0650138aa492ead
Manthan Kevadiya <manthankevadiya11@gmail.com>
2023-03-17 12:55:21 +0530

    Mathan

--- userDefault/ViewController.swift
+++ userDefault/ViewController.swift
@@ -1,19 +1,43 @@
 //
 //  ViewController.swift
-//  userDefault
+//  userDfaults
 //
-//  Created by R86 on 17/03/23.
+//  Created by R&W on 15/02/23.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField1: UITextField!
+    
+    @IBOutlet weak var textField2: UITextField!
+    
+    
+    @IBAction func buttonSave(_ sender: UIButton) {
+        showAlert()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
-
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let abc = segue.destination as! ViewController2
+        abc.a1 = textField1.text!
+        abc.a2 = textField2.text!
+    }
+    
+    func showAlert(){
+        let alert = UIAlertController(title: "save", message: "save", preferredStyle: .alert)
+        alert.addAction(UIAlertAction.init(title: "save", style: .default, handler: { _ in
+            UserDefaults.standard.set(self.textField1.text!,forKey: "userName")
+            UserDefaults.standard.set(self.textField2.text!,forKey: "password")
+            self.performSegue(withIdentifier: "cell", sender: self)
+        }))
+        alert.addAction(UIAlertAction.init(title: "cencal", style:.destructive, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
+
+
 
---
+++ userDefault/ViewController2.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController2.swift
+//  userDefault
+//
+//  Created by R86 on 17/03/23.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    @IBOutlet weak var label1: UILabel!
+       @IBOutlet weak var label2: UILabel!
+       var a1:String = ""
+       var a2:String = ""
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           label1.text = a1
+           label2.text = a2
+           
+       }
+       
+
+       
+
+   }
+
